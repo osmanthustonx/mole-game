b9642e4a2519b11c5da879b27ad7cd2f624d835f
K <vx3012564897@gmail.com>
2024-07-09 18:17:43 +0800

    feat: build game html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/mole-game/"
+  base: "/mole-game/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        game: 'game.html'
+      }
+    }
+  }
 });
